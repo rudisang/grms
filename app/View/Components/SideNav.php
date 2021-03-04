fbaff4b88e758667721e308a0b53d26816f958f4
Rudisang Morake <rk.morake18@gmail.com>
2021-03-04 22:16:49 +0200

    alpha v2.0.2

--- app/View/Components/SideNav.php
+++ app/View/Components/SideNav.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class sidenav extends Component
+class SideNav extends Component
 {
     /**
      * Create a new component instance.
